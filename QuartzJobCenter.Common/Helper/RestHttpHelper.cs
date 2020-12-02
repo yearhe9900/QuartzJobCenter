9cd73184cff3ec5318891f2e1fc28a3347603d8c
HeZhipeng <yearhe@outlook.com>
2020-12-02 16:56:21 +0800

    GET请求添加头部信息

--- QuartzJobCenter.Common/Helper/RestHttpHelper.cs
+++ QuartzJobCenter.Common/Helper/RestHttpHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using RestSharp;
 
@@ -31,7 +32,11 @@ namespace QuartzJobCenter.Common.Helper
             var uri = new Uri(url);
             var key = uri.Scheme + uri.Host;
             if (!dictionary.Keys.Contains(key))
-                dictionary.Add(key, new RestClient(uri.Scheme + "://" + uri.Host));
+            {
+                var client = new RestClient(uri.Scheme + "://" + uri.Host);
+                client.Timeout = 6000;//设置超时时间
+                dictionary.Add(key, client);
+            }
             return (dictionary[key], uri);
         }
 
@@ -43,27 +48,55 @@ namespace QuartzJobCenter.Common.Helper
         /// <param name="url">url地址</param>
         /// <param name="headers">webapi做用户认证</param>
         /// <returns></returns>
-        public async Task<HttpResponseMessage> GetAsync(string url, Dictionary<string, string> headers = null)
+        public async Task<IRestResponse> GetAsync(string url, Dictionary<string, string> headers = null)
         {
             var (client, uri) = GetRestClient(url);
             var localPath = uri.LocalPath;
-            var query = uri.Query.Substring(1);
+            var query = uri.Query[1..];
             var request = new RestRequest(localPath, Method.GET);//创建一个GET请求
             AddQueryParameters(request, query);
-            return null;
+            AddRequestHeader(request, headers);
+            //CancellationTokenSource cts = new CancellationTokenSource();
+            //cts.CancelAfter(10000);//请求超过指定时长后退出,避免长时间等待导致业务超时
+            //return await client.ExecuteAsync(request, cts.Token);
+            return await client.ExecuteAsync(request);
         }
 
         #endregion
 
         #region 私有方法 
 
-        public static void AddQueryParameters(RestRequest restRequest, string query)
+        /// <summary>
+        /// 请求添加参数
+        /// </summary>
+        /// <param name="restRequest"></param>
+        /// <param name="query"></param>
+        private void AddQueryParameters(RestRequest restRequest, string query)
         {
-            var paramArray = query.Split('&');
-            foreach (var paramStr in paramArray)
+            if (!string.IsNullOrWhiteSpace(query))
             {
-                var paramItems = paramStr.Split('=');
-                restRequest.AddParameter(paramItems[0], paramItems[1]);
+                var paramArray = query.Split('&');
+                foreach (var paramStr in paramArray)
+                {
+                    var paramItems = paramStr.Split('=');
+                    restRequest.AddParameter(paramItems[0], paramItems[1]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 请求添加头信息
+        /// </summary>
+        /// <param name="restRequest"></param>
+        /// <param name="query"></param>
+        private void AddRequestHeader(RestRequest restRequest, Dictionary<string, string> headers)
+        {
+            if (headers != null)
+            {
+                foreach (var header in headers)
+                {
+                    restRequest.AddHeader(header.Key, header.Value);
+                }
             }
         }
 
