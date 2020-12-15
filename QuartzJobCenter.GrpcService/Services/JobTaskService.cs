03b022e2c36326c4eb5be2ec697a6954e066b14a
HeZhipeng <yearhe@outlook.com>
2020-12-15 10:38:15 +0800

    添加GRPC项目

---
+++ QuartzJobCenter.GrpcService/Services/JobTaskService.cs
@@ -0,0 +1,27 @@
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuartzJobCenter.GrpcService
+{
+    public class JobTaskService : JobTask.JobTaskBase
+    {
+        private readonly ILogger<JobTaskService> _logger;
+        public JobTaskService(ILogger<JobTaskService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<ExcuteReply> Excute(ExcuteRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ExcuteReply
+            {
+                Message = "Hello " + request.Params,
+                Status = Stauts.Success
+            });
+        }
+    }
+}
